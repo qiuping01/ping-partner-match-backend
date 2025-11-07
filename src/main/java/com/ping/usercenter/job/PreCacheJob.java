8b6866eb8508173ee38a7ae03d0a800523fe5c17
qiuping01 <2187718660@qq.com>
2025-11-07 22:33:38 +0800

    主页 - Redisson实现分布式锁

--- src/main/java/com/ping/usercenter/job/PreCacheJob.java
+++ src/main/java/com/ping/usercenter/job/PreCacheJob.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ping.usercenter.model.domain.User;
 import com.ping.usercenter.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -29,26 +31,45 @@ public class PreCacheJob {
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Resource
+    private RedissonClient redissonClient;
+
     // 重点用户
     private List<Long> mainUserList = Arrays.asList(1L);
 
-
     // https://cron.ciding.cc/
-    @Scheduled(cron = "0 11 20 * * ? ")
+    @Scheduled(cron = "0 51 09 * * ? ")
     public void doCacheRecommendUser() {
-        for (Long userId : mainUserList) {
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
-            String redisKey = String.format("partner-match:user:recommend:%s"
-                    , userId);
-            ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-            // 写缓存
-            try {
-                // 30s 过期
-                valueOperations.set(redisKey, userPage, 30000,
-                        TimeUnit.MILLISECONDS);
-            } catch (Exception e) {
-                log.error("redis set key error", e);
+        RLock lock = redissonClient.getLock("partner-match:precachejob:docache:lock");
+        try {
+            // 只有一个线程能获取到锁
+            if (lock.tryLock(0, -1, TimeUnit.MILLISECONDS)) {
+                System.out.println("getLock: " + Thread.currentThread().getId());
+                for (Long userId : mainUserList) {
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
+                    String redisKey = String.format("partner-match:user:recommend:%s"
+                            , userId);
+                    ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+                    // 写缓存
+                    try {
+                        // 30s 过期
+                        // todo 定时删除有可能缓存血崩
+                        valueOperations.set(redisKey, userPage, 30000,
+                                TimeUnit.MILLISECONDS);
+                    } catch (Exception e) {
+                        log.error("redis set key error", e);
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            log.error("doCacheRecommendUser error", e);
+        } finally {
+            // 只能释放自己的锁
+            if (lock.isHeldByCurrentThread()){
+                System.out.println("unLock:  " + Thread.currentThread().getId());
+                lock.unlock();
             }
         }
     }
